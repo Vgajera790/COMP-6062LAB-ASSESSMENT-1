138892e59b784516b6192f32f3fcb866ebdf42d7
Vgajera790 <viralgajera123456@gmail.com>
2025-02-13 19:25:17 -0500

    Lab assessment 1

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+
+const studentFullname="Viral gajera";
+const studentNumber="1196217";
+print=`${studentFullname} - ${studentNumber}`;
+console.log(print);
+
+const headerContent = document.querySelector(`h1`);
+headerContent.innerHTML=print;
+headerContent.classList.add(`headingPrimary`);
+
+
+
+
